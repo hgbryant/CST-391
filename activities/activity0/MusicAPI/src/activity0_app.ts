0f4bad645dfda9be36fa73425aad8ccbc51f3885
hgbryant <huntergb1@icloud.com>
2026-03-05 17:41:45 -0600

    Update activity0_app.ts

--- activities/activity0/MusicAPI/src/activity0_app.ts
+++ activities/activity0/MusicAPI/src/activity0_app.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 const port = 3000;
-// Make sure you understand the following line of code. 
+ 
 app.get('/', (req: Request, res: Response) => {
  res.send('Hello TypeScript');
 });
